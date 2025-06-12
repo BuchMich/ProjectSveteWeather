2f1216e99e6d156a1723789cc48c54705d1483a9
BuchMich <mikebuc007@gmail.com>
2025-06-12 10:08:49 +0200

    Updates

--- _app/immutable/chunks/DYd9pbTk.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1ckm780)==null?void 0:s.base)??"/WeatherProjv3";var e;const t=((e=globalThis.__sveltekit_1ckm780)==null?void 0:e.assets)??a;export{t as a,a as b};
