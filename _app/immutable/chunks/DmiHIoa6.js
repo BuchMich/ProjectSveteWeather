adca667fafed7822cdfbfeb2518e7bca1b62bfe5
BuchMich <mikebuc007@gmail.com>
2025-06-12 09:43:51 +0200

    Updates

---
+++ _app/immutable/chunks/DmiHIoa6.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1rqskjc)==null?void 0:s.base)??"";var e;(e=globalThis.__sveltekit_1rqskjc)==null||e.assets;export{t as b};
---
+++ _app/immutable/chunks/DtMjN77N.js
@@ -0,0 +1,1 @@
+import{s as n,a as e}from"./CQPBOp8T.js";function s(t,i){t!=null&&typeof t.subscribe!="function"&&n(i)}function a(t){return t.toString=()=>(e(),""),t}export{a as p,s as v};
